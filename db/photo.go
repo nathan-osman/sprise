e2fea4e34634c2fc2b0044ffa8df2b4c2bbdd596
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-15 10:51:12 -0800

    Add photo URLs and Bucket foreign key to Photo model.

--- db/photo.go
+++ db/photo.go
@@ -9,8 +9,12 @@ type Photo struct {
 	ID       int64
 	Date     time.Time `gorm:"not null"`
 	Filename string    `gorm:"not null"`
+	URL      string    `gorm:"not null"`
+	ThumbURL string    `gorm:"not null"`
 	Width    int       `gorm:"not null"`
 	Height   int       `gorm:"not null"`
 	Camera   string    `gorm:"not null"`
+	Bucket   *Bucket   `gorm:"ForeignKey:BucketID"`
+	BucketID int64     `sql:"type:int REFERENCES buckets(id) ON DELETE CASCADE"`
 	Tags     []*Tag    `gorm:"many2many:photo_tags;"`
 }
