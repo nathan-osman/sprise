f6a085a816b1e84fb7d806b3e1e25fb2a09dff5d
Nathan Osman <nathan@quickmediasolutions.com>
2017-11-15 12:00:33 -0800

    Add render method for rendering templates.

---
+++ server/render.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/flosch/pongo2"
+)
+
+func (s *Server) render(w http.ResponseWriter, req *http.Request, templateName string, ctx pongo2.Context) {
+	t, err := s.templateSet.FromFile(templateName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	ctx["user"] = req.Context().Value(contextUser)
+	b, err := t.ExecuteBytes(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", strconv.Itoa(len(b)))
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
